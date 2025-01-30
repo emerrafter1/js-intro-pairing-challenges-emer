b90b6202a526411e28bfa87b75850541638a6f6a
zainab ali <zainab.ali056@outlook.com>
2025-01-30 16:56:03 +0000

    not completed

--- section-3/1-counter-spy.js
+++ section-3/1-counter-spy.js
@@ -7,8 +7,16 @@ The function should return an array containing the names of the people who aren'
 
 Recent intelligence has revealed that all spies codenames include the letters 's', 'p' or 'y'.
 You can't afford to take any chances, and all names that include those letters should be removed.
-  */
-}
+  if(people[i].includes('s')||people[i].includes('p')||people[i].includes('y'))*/
+let spies =[]
+ for (let i=people.length-1 ;i>=0;i--){
+  if(people[i].includes('s')||people[i].includes('p')||people[i].includes('y')){
+  people.splice(i,1)
+  }
+ } 
+
+ return people}
+
 
 console.log("counterSpy()");
 
@@ -16,7 +24,7 @@ runTest("returns an empty array if the only person is a spy", function () {
   check(counterSpy(["Daryl"])).isEqualTo([]);
 });
 
-skipTest("returns an array with all spys removed", function () {
+runTest("returns an array with all spys removed", function () {
   check(counterSpy(["Daryl", "Harriet", "James"])).isEqualTo(["Harriet"]);
   check(counterSpy(["Chris", "Daryl", "Harriet", "Mauro", "Sam"])).isEqualTo([
     "Harriet",
@@ -30,7 +38,7 @@ EXTRA CREDIT:
 Also, our spy admin team have asked that the names come back in alphabetical order, for spy filing purposes.
 So if you could do that you'd really be saving them a lot of work. Thanks.
 */
-skipTest("returns an array with names in alphabetical order", function () {
+runTest("returns an array with names in alphabetical order", function () {
   check(counterSpy(["Sam", "Harriet", "Adrian", "Mauro"])).isEqualTo([
     "Adrian",
     "Harriet",
