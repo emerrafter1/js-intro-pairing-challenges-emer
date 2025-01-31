69b746d90f5d8f224bc491c2cb7622feaf27c017
emerrafter17 <151878474+emerrafter17@users.noreply.github.com>
2025-01-31 08:18:07 +0000

    completed 3-find-missing-letter

--- section-3/3-find-missing-letter.js
+++ section-3/3-find-missing-letter.js
@@ -8,6 +8,58 @@ This function needs to take a list and needs to return the letter it is missing.
 
 You will always get a sorted array of consecutive letters, and it will always have exactly one letter missing. The length of the array will always be at least 2. The array will always contain letters in only one case.
   */
+
+const alphabet = {
+  a: 1, 
+  b:2,
+  c:3,
+  d:4,
+  e:5, 
+  f:6, 
+  g:7, 
+  h:8, 
+  i:9,
+  j:10,
+  k:11,
+  l:12, 
+  m:13, 
+  n:14,
+  o:15,
+  p:16, 
+  q:17, 
+  r:18, 
+  s:19, 
+  t: 20, 
+  u: 21, 
+  v: 22, 
+  w: 23, 
+  x:24, 
+  y:25,
+  z: 26
+}
+
+let currentValue = alphabet[letters[0].toLowerCase()]
+const isLowerCase = (alphabet[letters[0].toLowerCase()] === alphabet[letters[0]])
+
+for (let i = 0; i<letters.length; i++){
+  if(alphabet[letters[i].toLowerCase()] != currentValue){
+    const missingLetter = Object.keys(alphabet).find(key => alphabet[key] === currentValue)
+
+    if (isLowerCase){
+      return missingLetter
+    }
+    else {
+      return missingLetter.toUpperCase()
+    }
+
+  }
+  else {
+    currentValue++
+  }
+  
+
+}
+return ""
 }
 
 console.log("findMissingLetter()");
@@ -16,10 +68,11 @@ runTest("returns an empty string if no letters are missing", function () {
   check(findMissingLetter(["A", "B", "C", "D", "E"])).isEqualTo("");
 });
 
-skipTest("returns a missing capital letter", function () {
+runTest("returns a missing capital letter", function () {
+
   check(findMissingLetter(["A", "B", "C", "E"])).isEqualTo("D");
 });
 
-skipTest("returns a missing lower case letter", function () {
+runTest("returns a missing lower case letter", function () {
   check(findMissingLetter(["e", "f", "g", "i"])).isEqualTo("h");
 });
