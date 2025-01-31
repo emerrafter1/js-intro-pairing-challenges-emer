a03f06b1cd26931ddcf6208ad58985f84a9506f1
emerrafter17 <151878474+emerrafter17@users.noreply.github.com>
2025-01-31 21:32:59 +0000

    7-groupBy completed

--- section-3/7-groupBy.js
+++ section-3/7-groupBy.js
@@ -29,7 +29,38 @@ The function returns an object where the keys represent the matching values and 
   ]
 }
 */
-function groupBy(array, key) {}
+function groupBy(array, key) {
+
+  // take in an array of northcoders
+  // workout what the distinct values for that key are
+  //loops through set an create an array for that value
+  //loop through each northcoder and add them to an array for that key
+
+  let distinctGroups = {} //create an object for the groups
+  let distinctValues = new Set()
+
+  for(let i = 0; i< array.length; i++){
+    distinctValues.add(array[i][key])
+  }
+
+  console.log(distinctValues)
+
+  for(let value of distinctValues){ //create blank arrays with the distinct key
+    distinctGroups[value] = []
+  }
+
+
+
+  for (let i = 0; i<array.length; i++){
+    //distinctGroups[]
+    distinctGroups[array[i][key]].push(array[i]) 
+   
+  }
+
+return distinctGroups
+
+
+}
 
 console.log("groupBy()");
 
